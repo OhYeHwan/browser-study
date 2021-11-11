ab9daeac1f1cc26b8ca69635615948f2777bef99
OOHYEE <azss0470@gmail.com>
2021-11-11 11:23:36 +0900

    feat : checkbox

--- shoppingList/myProject/main.js
+++ shoppingList/myProject/main.js
@@ -1,27 +1,61 @@
-let item;
+window.onload = function () {
+  const items = document.querySelector(".items");
+  const input = document.querySelector(".footer__input");
+  const form = document.querySelector(".new-form");
 
-function input() {
-  item = document.querySelector("input").value;
-}
-
-function button_click() {
-  let p = document.createElement("p");
-  let btn = document.createElement("button");
-  let btnText = document.createTextNode("X");
-  btn.appendChild(btnText);
-  let node = document.createTextNode(item);
-  p.appendChild(node);
-  p.appendChild(btn);
-  const main = document.querySelector("main");
-  main.appendChild(p);
-  item = "";
-  let clear = document.querySelector("input");
-  clear.value = "";
-}
-
-function pressEnter() {
-  if (window.event.keyCode == 13) {
-    console.log(1);
-    button_click();
+  function onAdd() {
+    const text = input.value;
+    if (text === "") {
+      input.focus();
+      return;
+    }
+    const item = createItem(text);
+    items.appendChild(item);
+    item.scrollIntoView({ block: "center" });
+    input.value = "";
+    input.focus();
   }
-}
+
+  let id = 0;
+  function createItem(text) {
+    const itemRow = document.createElement("li");
+    itemRow.setAttribute("class", "item__row");
+    itemRow.setAttribute("data-id", id);
+    itemRow.innerHTML = `
+    <div class="item">
+        <input type="checkbox" id="${text}" class="item__checkbox">
+        <label for="${text}">${text}</label>
+            <button class="item__delete">
+              <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>
+    </div>
+    <div class="item__divider"></div>`;
+    id++;
+    return itemRow;
+  }
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    onAdd();
+  });
+
+  items.addEventListener("click", (event) => {
+    const id = event.target.dataset.id;
+    if (id) {
+      const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+      toBeDeleted.remove();
+    }
+
+    const cbox = document.querySelector(".item__checkbox");
+    if (cbox) {
+      const t = document.querySelector("label");
+      if (!cbox.checked) {
+        cbox.checked = true;
+        t.setAttribute("class", "item__name");
+      } else {
+        cbox.checked = false;
+        t.setAttribute("class", "");
+      }
+    }
+  });
+};
